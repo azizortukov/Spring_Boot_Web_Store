d03c0ecb5ca76719191deef0d3cab35e98bfd1a9
anas <azizortukov818@gmail.com>
2024-05-17 15:20:18 +0500

    initial commit

---
+++ src/main/java/uz/anas/web_store/SpringBootWebStoreApplication.java
@@ -0,0 +1,13 @@
+package uz.anas.web_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebStoreApplication.class, args);
+    }
+
+}
